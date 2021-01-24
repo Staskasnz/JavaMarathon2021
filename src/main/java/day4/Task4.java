52c944c4f7036239e49f7208a61048ae8e2992e9
Staskasnz <staskasnz@mail.ru>
2021-01-24 20:07:02 +0300

    Day4 complete исправил добавив цикл на 17-ой строчке

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -6,19 +6,22 @@ import java.util.Random;
 public class Task4 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] mass = new int[100];
+        int[] mass = new int[10];
         for (int i = 0; i < mass.length; i++) {
-            mass[i] = random.nextInt(10000);
+            mass[i] = random.nextInt(10);
         }
-        int sum;
+        int sum = 0;
         int sumfin = 0;
         int index = 0;
         for (int i = 0; i < mass.length - 2; i++) {
-            sum = mass[i] + mass[i + 1] + mass[i + 2];
+            for (int j = i; j < i + 3; j++) {
+                sum += mass[i];
+            }
             if (sum > sumfin) {
                 sumfin = sum;
                 index = i;
             }
+            sum = 0;
         }
         System.out.println("Ответ: " + index);
 
