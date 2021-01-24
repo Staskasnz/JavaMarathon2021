2ada645bd8629358e57c7740bf8dca39dde364f1
Staskasnz <staskasnz@mail.ru>
2021-01-24 22:15:23 +0300

    Day5 complete

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,37 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setRealise(1997);
+        car.setColor("Yellow");
+        car.setModel("Coup");
+        System.out.println(car.getRelease());
+        System.out.println(car.getColor());
+        System.out.println(car.getModel());
     }
 }
+    class Car{
+        private int release;
+        private String color;
+        private String model;
+
+        public int getRelease() {
+            return release;
+        }
+        public void setRealise(int realisex){
+            release = realisex ;
+        }
+        public String getColor(){
+            return color;
+        }
+        public void setColor(String color) {
+            this.color = color;
+        }
+        public String getModel(){
+            return model;
+        }
+        public void setModel(String model) {
+            this.model = model;
+        }
+    }
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,23 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike jamaha = new Motorbike(1955, "black", "new");
+        jamaha.getinfo();
     }
 }
+    class Motorbike{
+        private int realize;
+        private String color;
+        private String model;
+
+        public Motorbike(int realize, String color, String model) {
+            this.realize = realize;
+            this.color = color;
+            this.model = model;
+        }
+        public void getinfo(){
+                System.out.println(realize + " " + color + " " + model);
+            }
+        }
+
+
