ed0032836b08a8c0165f2ae7521ae52859f074be
Staskasnz <staskasnz@mail.ru>
2021-01-24 14:36:38 +0300

    Day4 complete исправил под условия 4-ю задачу

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -6,9 +6,9 @@ import java.util.Random;
 public class Task4 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] mass = new int[10];
+        int[] mass = new int[100];
         for (int i = 0; i < mass.length; i++) {
-            mass[i] = random.nextInt(10);
+            mass[i] = random.nextInt(10000);
         }
         int sum;
         int sumfin = 0;
