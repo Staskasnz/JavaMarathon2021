8c061d986f8ae7074a4674d0d6ad99ab870d6fe7
Staskasnz <staskasnz@mail.ru>
2021-01-29 00:50:53 +0300

    Day 9 complete

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,10 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Павел", "347");
+        Teacher teacher = new Teacher("Ольга Владимировна", "информатика");
 
+        student.printInfo();
+        teacher.printInfo();
     }
 }
