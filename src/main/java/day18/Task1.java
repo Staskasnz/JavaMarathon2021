cda451fc6db9333033c49f7b11c36c747fb84beb
Staskasnz <staskasnz@mail.ru>
2021-02-16 12:52:04 +0300

    Day 18

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -4,17 +4,17 @@ public class Task1 {
     public static void main(String[] args) {
 
         int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
-        System.out.println(recursionSum(numbers));
+        System.out.println(recursionSum(numbers, 0, 0));
     }
-    public static int sum;
-    public static int count;
-    public static int recursionSum(int[] num){
+
+    public static int recursionSum(int[] num, int count, int sum){
         sum += num[count];
         count++;
 
         if (count < num.length){
-            recursionSum(num);
+            return recursionSum(num, count, sum);
         }
+
         return sum;
     }
 }
